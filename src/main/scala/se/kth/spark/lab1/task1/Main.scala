6486c16a0391887a944010eb4b2064fc040f0bc5
ibbzy <isaid@kth.se>
2016-11-16 01:38:51 +0100

    Task 1,2 done

--- src/main/scala/se/kth/spark/lab1/task1/Main.scala
+++ src/main/scala/se/kth/spark/lab1/task1/Main.scala
@@ -1,36 +1,75 @@
 package se.kth.spark.lab1.task1
 
 import se.kth.spark.lab1._
-
 import org.apache.spark.ml.feature.RegexTokenizer
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.SQLContext
 
+import scala.collection.mutable.ListBuffer
+
 object Main {
+
+  case class Song(year: Double, f1: Double, f2: Double, f3: Double)
+
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("lab1").setMaster("local")
     val sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
 
     import sqlContext.implicits._
+    import org.apache.spark.sql.functions._
     import sqlContext._
 
+
     val filePath = "src/main/resources/millionsong.txt"
-    val rawDF = ???
+    val rawDF = sc.textFile(filePath).toDF()
 
     val rdd = sc.textFile(filePath)
 
     //Step1: print the first 5 rows, what is the delimiter, number of features and the data types?
 
+    //rdd.take(5).foreach(x=> println(x))
     //Step2: split each row into an array of features
-    val recordsRdd = rdd.map(???)
+    val recordsRdd = rdd.map(row => row.split(","))
 
-    //Step3: map each row into a Song object by using the year label and the first three features  
-    val songsRdd = recordsRdd.map(???)
+    // recordsRdd.take(5).foreach(x => println(x.mkString(" ")))
 
-    //Step4: convert your rdd into a datafram
-    val songsDf = ???
+    //Step3: map each row into a Song object by using the year label and the first three features
+    val songsRdd = recordsRdd.map(row => Song(row(0).toDouble,row(1).toDouble,row(2).toDouble,row(3).toDouble))
+
+    //Step4: convert your rdd into a dataframe
+    val songsDf = songsRdd.toDF()//("year","f1","f2","f3")
+
+
+    // Questions:
+    //1. How many are there in the dataset?
+    songsDf.registerTempTable("songs")
+    //println(songsRdd.count()) // RDD
+    println(songsDf.count())  // DataFrame
+    sqlContext.sql("SELECT COUNT(*) FROM songs").show()
+
+    //2. How many songs were released between 1998 & 2000
+
+    //println(songsRdd.filter(_.year >= 1998).filter( _.year<=2000).count())
+    println(songsDf.filter(songsDf("year") >= 1998 && songsDf("year") <= 2000).count())
+    sqlContext.sql("SELECT COUNT(*) FROM songs WHERE year>=1998 and year<=2000") .show()
+
+    //3. min, max and mean of the year column
+    val years = songsRdd.map(_.year)
+    println(years.min()+" "+years.max()+" "+years.mean()) // RDD
+    songsDf.agg(min("year"),max("year"),avg("year")).show() // DataFrame
+    sqlContext.sql("SELECT MIN(year),MAX(year),AVG(year) FROM songs").show()
+
+    //4. Show the number of songs per year between the years 2000 and 2010?
+    /*
+    val sMap =songsRdd.filter(_.year >= 2000).filter( _.year<=2010).map(song => (song.year,1))
+    val sReduce = sMap.reduceByKey(_+_)
+    sReduce.sortByKey().collect().foreach(println)
+    */
+
+    songsDf.filter(songsDf("year") >= 2000 && songsDf("year") <= 2010).groupBy("year").count().orderBy("year").show()
+    sqlContext.sql("SELECT year,count(year) FROM songs WHERE year>=2000 and year<=2010 GROUP BY year ORDER BY year").show()
   }
 }
--- src/main/scala/se/kth/spark/lab1/task2/Main.scala
+++ src/main/scala/se/kth/spark/lab1/task2/Main.scala
@@ -1,12 +1,15 @@
 package se.kth.spark.lab1.task2
 
 import se.kth.spark.lab1._
-
 import org.apache.spark.ml.feature.RegexTokenizer
+import org.apache.spark.ml.feature.VectorSlicer
 import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.linalg.Vector
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
+import org.apache.spark.ml.linalg.DenseVector
 import org.apache.spark.sql.SQLContext
+
 
 object Main {
   def main(args: Array[String]) {
@@ -18,43 +21,102 @@ object Main {
     import sqlContext._
 
     val filePath = "src/main/resources/millionsong.txt"
-    val rawDF = ???
+    val rawDF = sc.textFile(filePath).toDF("raw")
+    //rawDF.show(3)
 
     //Step1: tokenize each row
     val regexTokenizer = new RegexTokenizer()
-      .setInputCol(???)
-      .setOutputCol(???)
-      .setPattern(???)
+      .setInputCol("raw")
+      .setOutputCol("words")
+      .setPattern("\\W")
 
     //Step2: transform with tokenizer and show 5 rows
-    ???
-
+    val regDF  = regexTokenizer.transform(rawDF)
+    //regDF.limit(5).show()
     //Step3: transform array of tokens to a vector of tokens (use our ArrayToVector)
+
     val arr2Vect = new Array2Vector()
-    ???
+      .setInputCol("words")
+      .setOutputCol("vector")
+
+
+    val vecDf = arr2Vect.transform(regDF)
+    //vecDf.show(3)
+
+
+    //vRegDf.rdd.map(row =)
+    //vRegDf.select(vRegDf.col("vector").
+    //vRegDf.createOrReplaceTempView("table")
+    //sqlContext.udf.register("yearUDF", (x:Vector[String]) => x.apply(0))
+    //val query = "SELECT yearUDF(vector) AS year FROM table"
+    //sql(query).show()
+    // vRegDf.withColumn("firstValue", callUDF("yearUDF",vRegDf.col("vector")))
+    //vRegDf.select($"id", callUDF("yearUDF", $"value"))
 
     //Step4: extract the label(year) into a new column
-    val lSlicer = ???
+    //vRegDf.map({ case Row(v: Vector[String]) => v(0) })
+    //val r = vRegDf.rdd
+
+    //val year = r.map(row => row.getAs[DenseVector]("vector").values(0)).toDF("year")
+    //val yearvec= vec("year")
+    //println(year.dtypes(0))
+    //print(vec.count())
+    //print(vRegDf.count())
+    //val lSlicer = vec
+    //vRegDf.withColumn("year",yearvec)
+    //vRegDf.drop("raw","words").collect()
+    val lSlicer = new VectorSlicer()
+      .setInputCol("vector")
+      .setOutputCol("labelV")
+      .setIndices(Array(0))
+
+    val lDf = lSlicer.transform(vecDf)
+
+    //lDf.show(3)
 
     //Step5: convert type of the label from vector to double (use our Vector2Double)
-    val v2d = new Vector2DoubleUDF(???)
-    ???
-    //Step6: shift all labels by the value of minimum label such that the value of the smallest becomes 0 (use our DoubleUDF) 
-    val lShifter = new DoubleUDF(???)
-    ???
+    //val yo=udf {(s:DenseVector) => s.values(0)}
+    val v2d = new Vector2DoubleUDF({(v: Vector) => v.apply(0)})
+      .setInputCol("labelV")
+      .setOutputCol("labelD")
+
+    val dDf = v2d.transform(lDf)
+    //dDf.show(3)
+
+
+
+    //
+    //Step6: shift all labels by the value of minimum label such that the value of the smallest becomes 0 (use our DoubleUDF)
+
+    val minYear = 1922.0
+    val lShifter = new DoubleUDF({(d: Double) => d-minYear})
+      .setInputCol("labelD")
+      .setOutputCol("label")
+
+    val sDf = lShifter.transform(dDf)
+    //sDf.show(3)
+
     //Step7: extract just the 3 first features in a new vector column
-    val fSlicer = ???
+    val fSlicer = new VectorSlicer()
+      .setInputCol("vector")
+      .setOutputCol("features")
+      .setIndices(Array(1,2,3))
+
+    val fDf = fSlicer.transform(sDf)
+
+
 
     //Step8: put everything together in a pipeline
-    val pipeline = new Pipeline().setStages(???)
+    val pipeline = new Pipeline().setStages(Array(regexTokenizer,arr2Vect,lSlicer,v2d,lShifter,fSlicer))
 
     //Step9: generate model by fitting the rawDf into the pipeline
     val pipelineModel = pipeline.fit(rawDF)
 
     //Step10: transform data with the model - do predictions
-    ???
+    val pDF = pipelineModel.transform(rawDF)
 
     //Step11: drop all columns from the dataframe other than label and features
-    ???
+    val finalDF = pDF.select("features","label")
+    finalDF.show(3)
   }
 }
